8a19f2c4fa0edb46bf76e50fba45618c8891eb41
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-04-09 10:21:15 +0400

    Task Finished

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public static class ArraySum
+    {
+        public static int[] Add(this int[] arr, int num)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+            return arr;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[0];
+            arr = arr.Add(1);
+            arr = arr.Add(2);
+            arr = arr.Add(3);
+            arr = arr.Add(10);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
